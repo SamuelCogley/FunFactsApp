364cf304eb7a315ea3f4169c990aa719f9f78c31
risaac <risaac@ncsu.edu>
2018-11-12 23:06:59 -0500

    Set up layout of app

---
+++ app/src/main/java/com/example/randomfacts/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.randomfacts;
+
+import android.service.autofill.TextValueSanitizer;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView
+
+public class MainActivity extends AppCompatActivity {
+    // Declare our View variable
+    private TextView factTextView;
+    private Button showFactButton;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Assign the Views from the layout file to the corresponding variables
+
+    }
+}
